71088ddcb2e8239d9736ddb0129d4ecceb2fb524
Rob McKenzie <mckenro@gmail.com>
2017-04-07 15:54:11 -0700

    add patch and delete routes for brands and stores

--- app/app.php
+++ app/app.php
@@ -1,4 +1,5 @@
 <?php
+  date_default_timezone_set('America/Los_Angeles');
   require_once __DIR__."/../vendor/autoload.php";
   require_once __DIR__."/../src/Brand.php";
   require_once __DIR__.'/../src/Store.php';
@@ -13,8 +14,91 @@
     'twig.path' => __DIR__.'/../views'
   ));
 
+  use Symfony\Component\HttpFoundation\Request;
+  Request::enableHttpMethodParameterOverride();
+
   $app->get("/", function() use ($app) {
-    return $app['twig']->render('index.html.twig');
+    return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
   });
+
+  $app->post("/stores", function() use ($app) {
+    $store = new Store($_POST['store']);
+    $store->save();
+    return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+  });
+
+  $app->post("/brands", function() use ($app) {
+    $brand = new Brand($_POST['brand']);
+    $brand->save();
+    return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+  });
+
+  $app->post("/delete_stores", function() use ($app) {
+    Store::deleteAll();
+    return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+  });
+
+  $app->post("/delete_brands", function() use ($app) {
+    Brand::deleteAll();
+    return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+  });
+
+  // ---------------------------------------
+  // store update and delete below this line
+
+  $app->get("/store_edit/{id}", function($id) use ($app) {
+    $current_store = Store::find($id);
+    $store_brands = $current_store->getBrands();
+    return $app['twig']->render('store_edit.html.twig', array('stores' => $current_store, 'brands' => $store_brands));
+  });
+
+  $app->patch("/store_edit/{id}", function($id) use ($app) {
+    $current_store = Store::find($id);
+    $new_name = $_POST['store_edit'];
+    $current_store->update($new_name);
+    $store_brands = $current_store->getBrands();
+    return $app['twig']->render('store_edit.html.twig', array('stores' => $current_store, 'brands' => $store_brands));
+  });
+
+  $app->delete("/store_edit/{id}", function($id) use ($app) {
+    $current_store = Store::find($id);
+    $current_store->delete();
+    return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+  });
+
+  // ---------------------------------------
+  // brand update and delete below this line
+
+  $app->get("/brand_edit/{id}", function($id) use ($app) {
+    $current_brand = Brand::find($id);
+    $brand_stores = $current_brand->getStores();
+    return $app['twig']->render('brand_edit.html.twig', array('brands' => $current_brand, 'stores' => $brand_stores));
+  });
+
+  $app->patch("/brand_edit/{id}", function($id) use ($app) {
+    $current_brand = Brand::find($id);
+    $new_name = $_POST['brand_edit'];
+    $current_brand->update($new_name);
+    $brand_stores = $current_brand->getStores();
+    return $app['twig']->render('brand_edit.html.twig', array('brands' => $current_brand, 'stores' => $brand_stores));
+  });
+
+  $app->delete("/brand_edit/{id}", function($id) use ($app) {
+    $current_brand = Brand::find($id);
+    $current_brand->delete();
+    return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+  });
+
+  // ---------------------------------------
+  // add store(s) to brand below this line
+
+  $app->post("/brand_edit/{id}", function($id) use ($app) {
+    $current_brand = Brand::find($id);
+    $new_store = $_POST['add_store'];
+    $current_brand->addStore($new_store);
+    return $app['twig']->render('brand_edit.html.twig', array('brands' => $current_brand, 'stores' => $brand_stores));
+  });
+
+
   return $app;
  ?>
